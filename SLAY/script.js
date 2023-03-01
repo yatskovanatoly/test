aea27cecef9bc6827eecb374c273d44dcaa67b39
Anatoly Yatskov <yatskovanatoly@gmail.com>
2023-03-01 06:57:43 +0300

    organized beautifully

--- SLAY/script.js
+++ SLAY/script.js
@@ -1,74 +1,99 @@
-const blocks = document.querySelectorAll(".over");
-const blocks2 = document.querySelectorAll(".text");
+let words = ['slay', 'you go gurl', 'psichiatria']; // массив исходных слов
+let usedWords = []; // массив использованных слов
+let lastWord // последнее использованное слово
 
-blocks.forEach(block => {
-  block.addEventListener("mouseover", function (){
-    blocks2.forEach (blockx => {
-      blockx.style.color = "rgba(0,0,0,0)";
+// функция задают реакцию после наведение мышки на поле поверх текста
+function overlayMethod() {
+  
+  const blocks = document.querySelectorAll(".over");
+  const blocks2 = document.querySelectorAll(".text");
+
+  blocks.forEach(block => {
+    block.addEventListener("mouseover", function () {
+      blocks2.forEach(blockx => {
+        blockx.style.color = "rgba(0,0,0,0)";
+      });
+    });
+    block.addEventListener("mouseleave", function () {
+      blocks2.forEach(blocky => {
+        blocky.style.color = "black";
+      });
     });
   });
-  block.addEventListener("mouseleave", function () {
-    blocks2.forEach (blocky => {
-      blocky.style.color = "black";
-    });
-  });
-});
+}
 
+// функция возращает использованные слова в исходный массив, если тот пуст
+function ifWordsEmpty(arr, arr2) {
+  
+  if (arr.length === 0) { // если не осталось неиспользованных слов
+    
+    arr.push(...arr2); // пушим использованные слова в исходный массив
+    arr2.splice(0, arr2.length); // очищаем массив использованных слов
+    console.log('NEW CYCLE'); // уведомляем о начале нового цикла
+    
+  }
+}
 
+// функция проверяет наличие слов в исходном массиве и совпадает ли выбранное слово с последним использованным
+function ifWordsNotEmpty(arr, arr2, text, randomIndex) {
 
+  if (arr.length > 0) {
 
+    text.textContent = arr[randomIndex]; // обновляем текст на странице
 
-// let words = ['slay', 'you go gurl', 'psichiatria'];
-let words = ['A', 'B', 'C']; // даём коду массив откуда брать слово
-let usedWords = []; // даём коду массив куда убирать использованное слово
-var lastWord // переменная последнего использованного слова
+    if (arr2 === 0) {
+      console.log('WORDS: ' + arr); // печатаем в консоль массив слов если использованных ещё нет
+    }
+    
+    arr2.push(arr[randomIndex]); // копируем использованное слово в свой массив
+    arr.splice(randomIndex, 1); // удаляем использованное слово из массива слов
+    console.log('USED: ' + arr2); // логим использованные слова
 
+    // печатаем в консоль оставшиеся слова, если они есть
+    if (arr.length > 0) {
+      console.log('WORDS: ' + arr);
+    }
 
-function getRI() { // функция чтобы получить случайный индекс для выбора слова из массива
-  const max = words.length; // говорим что максимум это всегда длинна массива
-  const randomIndex = Math.floor(Math.random() * (max));
-  // console.log(randomIndex)
+    lastWord = arr2[arr2.length - 1]; // декларируем последнее слово
+  
+  }
+}
+
+// функция проверяет повторяет ли слово по новому индексу последнее использованное слово
+function checkRepetition(arr, randomIndex) {
+  
+  while (lastWord === (arr[randomIndex])) {
+    console.log('REPETITION FOUND: ' + lastWord);
+    randomIndex = getRandomIndex(arr); // получаем индекс
+  }
   return randomIndex;
 }
 
-function updateText() { // ОГРОМНАЯ ФУНКЦИЯ ОБНОВЛЕНИЯ ТЕКСТА
-  let text = document.querySelector('.text');
-
+// функция генерирует случайный индекс для выбора слова из массива
+function getRandomIndex(arr) {
   
-  let randomIndex = getRI();
+  const max = arr.length; // определяем максимальный индекс длинной массива
+  const randomIndex = Math.floor(Math.random() * (max));
+  return randomIndex;
 
-  if (words.length == 0) { // если не осталось неиспользованных слов
-    words.push(...usedWords) // пушим использованные обратно домой
-    usedWords.splice(0, usedWords.length) // очищаем массив использованных
-    console.log('LEFT: 0, NEW CYCLE') // увеломляем
-    // console.log(words[randomIndex]) 
-    // console.log(lastWord)
-  }
-  randomIndex = getRI(); // иначе почти всё время 0, индекс не обновляется
-  
-   while (lastWord == (words[randomIndex])) {
-    console.log('REPETITION FOUND: ' + lastWord)
-    randomIndex = getRI() // получаем индекс
-  };
-
-
-  if (words.length > 0) {
-    // смотрим есть ли что-то в массиве слов и не совпадает ли выбранное слово с последним использованным
-    
-    text.textContent = words[randomIndex]; // здесь обновляем текст на странице, !НЕ РАБОТАЕТ В ВСКОДЕ!
-
-    if (usedWords <= 0) { console.log('WORDS: ' + words) }; // логим массив слов если использованных ещё нет
-    
-    usedWords.push(words[randomIndex]); // копируем использованное слово в свой массив
-    words.splice(randomIndex, 1) // удаляем использованное слово из массива слов
-    console.log('USED: ' + usedWords) // логим использованные слова
-    
-    if (words.length > 0) { console.log('WORDS: ' + words) } // если слова ещё есть логим оставшиеся
-    lastWord = usedWords[usedWords.length - 1] // декларируем последнее слово
-
-  }
 }
 
-setInterval(() => updateText(), 1000); // повторяем обновление текста очень часто
-// setTimeout(() => { clearInterval(timerId); }, 1000); 
-   // откомментить чтобы остановить обновление через секунду после начала
+// функция обновляет текст на странице, запуская функции проверок
+function updateText() {
+
+  let text = document.querySelector('.text');
+
+  ifWordsEmpty(words, usedWords);
+
+  let randomIndex = getRandomIndex(words);
+
+  randomIndex = checkRepetition(words, randomIndex);
+
+  ifWordsNotEmpty(words, usedWords, text, randomIndex);
+
+}
+
+overlayMethod();
+updateText();
+let timerId = setInterval(() => updateText(), 10000); // повторяет обновление текста через заданный интервал
+// setTimeout(() => { clearInterval(timerId); }, 5000); // останавливает обновление через заданное время
